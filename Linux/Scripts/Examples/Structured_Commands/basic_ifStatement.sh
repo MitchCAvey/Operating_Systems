aee2e883948fe23928db550ec008dbb2cb91c3ea
mitchavey <158223733+MitchCAvey@users.noreply.github.com>
2025-07-02 08:52:13 -0400

    Adding the first few scripts that show case the basics of the if-then statement

---
+++ Linux/Scripts/Examples/Structured_Commands/basic_ifStatement.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This is a script showing the basic's of using the if-then statement
+
+if pwd
+then
+	echo "It worked"
+fi
---
+++ Linux/Scripts/Examples/Structured_Commands/basic_ifStatement2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#This script shows what happens when a bad command if provide to the if statement
+
+if IamNotACommand
+then
+	echo "It Worked!"
+fi
+
+echo "We are outside the if statement
