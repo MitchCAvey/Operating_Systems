c9b6d888edfa2d188b2c06f9422a5fe2187f8f7a
mitchavey <158223733+MitchCAvey@users.noreply.github.com>
2025-07-08 22:52:50 -0400

    Adding more scripts related to Logic Flow Control

---
+++ Linux/Scripts/Examples/Structured_Commands/LogicFlowControl/TestConditionComparisons/test-NumericComparisons2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Using floating point numbers in test evaulations
+
+value1=5.555
+
+echo "The test value is $value1"
+
+if [ $value -gt 5 ]
+then
+	echo "the test value $value1 is greater than 5"
+fi
+#
