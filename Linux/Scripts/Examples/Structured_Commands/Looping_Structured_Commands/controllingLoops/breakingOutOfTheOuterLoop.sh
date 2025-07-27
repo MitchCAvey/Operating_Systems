56dda7bb324423e11c8de312966a6b9252aa7b61
mitchavey <158223733+MitchCAvey@users.noreply.github.com>
2025-07-27 19:57:50 -0400

    I made changes to the location of some files. I'm also adding new scripts using the break and continue commands

---
+++ Linux/Scripts/Examples/Structured_Commands/Looping_Structured_Commands/controllingLoops/breakingOutOfTheOuterLoop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Breaking out of an outer loop
+#
+
+for (( a = 1; a < 4; a++ ))
+do
+    echo "Outer Loop: $a"
+
+    for (( b = 1; b < 100; b++ ))
+    do
+	if [ $b -gt 4 ]
+	then
+	    break 2
+	fi
+
+	echo "	Inner Loop: $b"
+    done
+done
---
+++ Linux/Scripts/Examples/Structured_Commands/Looping_Structured_Commands/controllingLoops/improperlyUsingContinueWithWhileLoops.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Improperly using the continue command in a while loop
+# IMPORTANT: MAKE SURE TO PIPE THIS SCRIPT TO THE MORE COMMAND 
+#
+
+var1=0
+
+while echo "while iteration: $var1"
+    [ $var1 -lt 15 ]
+do
+    if [ $var1 -gt 5 ] && [ $var1 -lt 10 ]
+    then
+	continue
+    fi
+
+    echo "	Inside iteration number: $var1"
+    var1=$[ $var1 + 1 ]
+done
+
